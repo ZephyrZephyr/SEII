eda40edbfca69d65068847b6ba9f1dedeef4ae53
ZephyrZephyr <374698133@qq.com>
2015-12-29 22:36:20 +0800

    //

--- SEII/src/main/java/BLService/DeliveryCenterStockBLService.java
+++ SEII/src/main/java/BLService/DeliveryCenterStockBLService.java
@@ -3,6 +3,7 @@ package BLService;
 import java.util.ArrayList;
 
 import vo.AlarmVO;
+import vo.GoodInStockVO;
 import vo.InventoryCheckVO;
 import vo.InventoryCountVO;
 import vo.TotalWarehouseVO;
@@ -11,4 +12,5 @@ public interface DeliveryCenterStockBLService {
 	public abstract TotalWarehouseVO getTotalWarehouse(ArrayList<InventoryCheckVO> invClist);
 	public abstract void exportInventoryCount(ArrayList<InventoryCountVO> invClist);
 	public abstract ArrayList<AlarmVO> checkSituration(String percent,ArrayList<AlarmVO> alarmlist);
+	public abstract ArrayList<GoodInStockVO> getGoodsByPosition(String unit,String row,String line);
 }
--- SEII/src/main/java/deliveryCenterStockBL/DeliveryCenterStockBLController.java
+++ SEII/src/main/java/deliveryCenterStockBL/DeliveryCenterStockBLController.java
@@ -1,16 +1,19 @@
 package deliveryCenterStockBL;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import data.DeliveryCenterStockData;
 import dataService.DeliveryCenterStockDataService;
 import vo.AlarmVO;
+import vo.GoodInStockVO;
 import vo.InventoryCheckVO;
 import vo.InventoryCountVO;
 import vo.TotalWarehouseVO;
 import BLService.DeliveryCenterStockBLService;
 
 public class DeliveryCenterStockBLController implements DeliveryCenterStockBLService{
+	DeliveryCenterStockDataService data;
 
 	public TotalWarehouseVO getTotalWarehouse(ArrayList<InventoryCheckVO> invClist) {
 		double in = 0;
@@ -27,8 +30,13 @@ public class DeliveryCenterStockBLController implements DeliveryCenterStockBLSer
 	}
 
 	public void exportInventoryCount(ArrayList<InventoryCountVO> invClist) {
-		DeliveryCenterStockDataService data = new DeliveryCenterStockData();
-		data.ExportInventoryCountTable(invClist);
+		try {
+			data = new DeliveryCenterStockData();
+			data.ExportInventoryCountTable(invClist);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public ArrayList<AlarmVO> checkSituration(String percent,ArrayList<AlarmVO> alarmlist) {
@@ -38,6 +46,17 @@ public class DeliveryCenterStockBLController implements DeliveryCenterStockBLSer
 			alarmlist.set(i, temp);
 		}
 		return alarmlist;
+	}
+
+	public ArrayList<GoodInStockVO> getGoodsByPosition(String unit, String row,
+			String line) {
+		try {
+			data = new DeliveryCenterStockData();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return data.getGoodsByPosition(unit, row, line);
 	}
 
 }
